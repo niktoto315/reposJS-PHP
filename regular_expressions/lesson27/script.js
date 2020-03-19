ea9dc66162b36aff81aaeda7331353a736d9b011
niktoto315 <62284029+niktoto315@users.noreply.github.com>
2020-03-19 20:11:36 +0700

    Add files via upload

---
+++ regular_expressions/lesson27/script.js
@@ -0,0 +1,5 @@
+let str = '1 23 456 789';
+
+//27.1
+let res = str.search(/\d{3}/);
+console.log(res);
---
+++ regular_expressions/lesson28/script.js
@@ -0,0 +1,5 @@
+let str = '2025-12-31 12:59:59';
+
+//28.1
+let res = str.split(/[- :]/);
+console.log(res);
