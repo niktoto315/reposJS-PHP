c50d9c6e88a1902d825c3df573ec1d01f643dbf5
niktoto315 <62284029+niktoto315@users.noreply.github.com>
2020-03-19 17:51:21 +0700

    Add files via upload

---
+++ regular_expressions/lesson19/script.js
@@ -0,0 +1,11 @@
+let str = 'site.ru sss site.com zzz site.net';
+let sum = 0;
+
+//19.1
+console.log(str.match(/\w+\.ru|\w+\.com|\w+\.net/g, '!'));
+//19.2
+str = 'a1b c34d x567z';
+for(let i of str.match(/\d/g)){
+	sum += +i;
+}
+console.log(sum);
