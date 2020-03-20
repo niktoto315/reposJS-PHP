ee23b3e876931b55b624acd14eb36a8454f2a740
niktoto315 <62284029+niktoto315@users.noreply.github.com>
2020-03-20 15:49:01 +0700

    Update script.js

--- regular_expressions/lesson19/script.js
+++ regular_expressions/lesson19/script.js
@@ -5,7 +5,7 @@ let sum = 0;
 console.log(str.match(/\w+\.ru|\w+\.com|\w+\.net/g, '!'));
 //19.2
 str = 'a1b c34d x567z';
-for(let i of str.match(/\d/g)){
+for(let i of str.match(/\d+/g)){
 	sum += +i;
 }
 console.log(sum);
