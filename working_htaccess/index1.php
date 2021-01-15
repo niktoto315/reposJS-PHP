edfd6c573b4a73dcbf506f2650f70613739feda7
niktoto315 <62284029+niktoto315@users.noreply.github.com>
2021-01-16 00:26:05 +0700

    Add files via upload

---
+++ working_htaccess/index1.php
@@ -0,0 +1,6 @@
+<?php
+	echo 'index<br>';
+	var_dump($_SERVER['REQUEST_URI']);
+	echo '<br>';
+	var_dump($_GET['get']);
+?>
---
+++ working_htaccess/test.php
@@ -0,0 +1,4 @@
+<?php
+	echo 'test<br>';
+	var_dump($_SERVER['REQUEST_URI']);
+?>
